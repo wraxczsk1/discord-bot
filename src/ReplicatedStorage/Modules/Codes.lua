dd3044baa587abacb03c21abcc0aba1607d0abfc
Baitters <kudelka.zdenek0@gmail.com>
2026-07-13 14:23:34 +0200

    Delete src/ReplicatedStorage/Modules directory

--- src/ReplicatedStorage/Modules/Codes.lua
+++
@@ -1,5 +0,0 @@
-return {
-    FREECOINS = {RewardType="Money", Amount=100, OneTime=true},
-    CHILLFARM = {RewardType="Money", Amount=200, OneTime=true},
-    STARTERPACK = {RewardType="Bundle", Money=150, Multiplier=1.1, OneTime=true}
-}
